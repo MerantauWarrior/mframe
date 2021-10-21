c05faa37605dd006024e81fc25b7d5e448e00cba
boriskleshch <b.kleshch@bsg.world>
2021-10-21 21:37:27 +0300

    modal and improvements 3

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -48,20 +48,12 @@ $(document).ready(function () {
     $('.modal').fadeIn(250);
   });
 
-})
-
-
-document.addEventListener("DOMContentLoaded", function () {
-
-  document.getElementsByClassName('nav-btn')[0].addEventListener('click', function () {
-    if (this.classList.contains('nav-btn_opened')){
-      this.classList.remove('nav-btn_opened');
-      document.getElementsByClassName('navigation')[0].classList.remove('navigation_opened');
-    }else {
-      this.classList.add('nav-btn_opened');
-      document.getElementsByClassName('navigation')[0].classList.add('navigation_opened');
-    }
+  // navigation
+  $('.nav-btn').click(function () {
+    $('.navigation').addClass('navigation_opened');
+  })
+  $('.nav-close').click(function () {
+    $('.navigation').removeClass('navigation_opened');
   })
 
-
-});
+})
